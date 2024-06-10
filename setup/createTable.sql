8687d9ce9c591c9942f6350a4410d093e0cffe81
Burke Holland <burkeholland@gmail.com>
2024-06-10 14:07:47 +0000

    chore: Update index.html, package.json, createTable.sql, and setup.sh

--- setup/createTable.sql
+++ setup/createTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE vehicles (
     vin VARCHAR(255) PRIMARY KEY,
     county VARCHAR(255),
     city VARCHAR(255),
-    state VARCHAR(2),
+    state CHAR(2),
     postal_code VARCHAR(10),
     model_year INT,
     make VARCHAR(255),
@@ -12,8 +12,5 @@ CREATE TABLE vehicles (
     electric_range INT,
     base_msrp INT,
     legislative_district INT,
-    dol_vehicle_id BIGINT,
-    vehicle_location VARCHAR(255),
-    electric_utility VARCHAR(255),
-    census_tract_2020 VARCHAR(255)
+    dol_vehicle_id BIGINT
 );
