964d3e6127584583c6ad6e7d6e09abd07b7fa084
Burke Holland <burkeholland@gmail.com>
2024-06-10 14:14:58 +0000

    Remove files

--- routers/vehicleRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllVehicles } from '../controllers/vehicleController';
-
-const vehicleRouter = express.Router();
-
-vehicleRouter.get('/', getAllVehicles);
-
-export default vehicleRouter;
